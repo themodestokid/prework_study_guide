c0f27fbc8075212dad83fcf059f09979e8c0bf91
Jeremy Osner <anacreon@gmail.com>
2024-10-21 09:34:36 -0400

    add javascript function calls

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,27 @@
 const topics = [ "HTML", "CSS", "Git", "JavaScript" ];
 console.log("topics:" + topics);
-for (let i = 0; i < topics.length; ++i) {
-    console.log(topics[i])
+
+function listTopics() {
+    for (let i = 0; i < topics.length; ++i) {
+        console.log(topics[i])
+    }
 }
 
-let topic = "HTML";
-
-topic = 'CSS';
-
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+function selectTopic(topic) {
+    if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
 }
+
+console.log("Here are the topics we reviewed in the pre-work:")
+listTopics();
+console.log("Which topic should we study first?")
+selectTopic(topics[Math.floor(Math.random() * topics.length)]);
